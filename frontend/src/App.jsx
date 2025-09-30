5239bf52f5f71528330b58f4f969ef494b95f8f1
Pedro Peixoto <bonnieevil86@gmail.com>
2025-09-30 10:28:57 -0300

    Complete filters now

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,10 +3,10 @@ import Viewer from './components/Viewer'
 
 function App() {
   const [filters, setFilters] = useState({
-    name: '',
+    search: '',
     min_price: '',
     max_price: '',
-    sort: ''
+    ordering: ''
   });
 
   const handleChange = (e) => {
@@ -16,11 +16,14 @@ function App() {
   return (
     <>
       <main className="container mx-auto p-4 bg-slate-800 min-h-screen min-w-screen">
-        <div className='p-4'>
-          <select name="sort" value={filters.sort} onChange={handleChange} className="p-2 rounded bg-white">
-            <option value="">Sort By</option>
-            <option value="-price">Price -</option>
-            <option value="price">Price +</option>
+        <div className='p-4 flex gap-2'>
+          <input type='text' name="search" value={filters.search} onChange={handleChange} placeholder="Product Name" className="p-2 rounded bg-white" />
+          <input type='number' name="min_price" value={filters.min_price} onChange={handleChange} placeholder="Min Price" className="p-2 rounded bg-white" />
+          <input type='number' name="max_price" value={filters.max_price} onChange={handleChange} placeholder="Max Price" className="p-2 rounded bg-white" />
+          <select name="ordering" value={filters.ordering} onChange={handleChange} className="p-2 rounded bg-white">
+            <option value="">Ordering By</option>
+            <option value="-price">Price +</option>
+            <option value="price">Price -</option>
           </select>
         </div>
         <Viewer filters={filters} />
